5e4c076940d38f113b97b0ecfbdde255a67054c0
oywe <savory_pretax.20@icloud.com>
2026-02-16 21:50:46 +0900

    Create test_first.py

---
+++ tests/test_first.py
@@ -0,0 +1,12 @@
+from HiHello import respond
+import pytest
+
+def test_hi():
+    assert respond("Hi") == "Hello"
+
+def test_hello():
+    assert respond("Hello") == "Hi"
+
+def test_error():
+    with pytest.raises(ValueError):
+        respond("Bye")
